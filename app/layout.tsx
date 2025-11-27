8ed1306dc6cda5ec63801d83502e6abc251b06d7
Giulex84 <giulianom.gm@gmail.com>
2025-11-27 02:27:00 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import "./globals.css";
 import { IOUProvider } from "@/components/iou-context";
-import type { Metadata } from "next";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "IOU Ledger Pro",
   description: "Track your IOUs easily",
 };
@@ -14,6 +13,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          src="https://sdk.minepi.com/pi-sdk.js"
+          integrity="sha384-8qts0jqAbKLFXrrHWlP88eCjcw0bmFUXZpGgUa7mbFkFzfA2seQhLcS6grwFzeW9"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>
         <IOUProvider>{children}</IOUProvider>
       </body>
