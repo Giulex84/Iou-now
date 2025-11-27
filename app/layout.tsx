f7cb55328a0f19dc0cb9e7f83fe56a8e798f3cff
Giulex84 <giulianom.gm@gmail.com>
2025-11-27 02:31:55 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,24 @@
+// app/layout.tsx  (SERVER COMPONENT – nessun "use client")
+
 import "./globals.css";
-import { IOUProvider } from "@/components/iou-context";
 
 export const metadata = {
   title: "IOU Ledger Pro",
   description: "Track your IOUs easily",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        {/* Pi SDK load */}
         <script
           src="https://sdk.minepi.com/pi-sdk.js"
           integrity="sha384-8qts0jqAbKLFXrrHWlP88eCjcw0bmFUXZpGgUa7mbFkFzfA2seQhLcS6grwFzeW9"
           crossOrigin="anonymous"
         />
       </head>
-      <body>
-        <IOUProvider>{children}</IOUProvider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
