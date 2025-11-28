fb632c32104fc14b3a8eabc987f0c8212dbc13d8
Giulex84 <giulianom.gm@gmail.com>
2025-11-28 16:34:34 +0100

    Update HistoryPage.tsx

--- components/HistoryPage.tsx
+++ components/HistoryPage.tsx
@@ -1,35 +1,20 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
 import HistoryContent from "@/components/HistoryContent";
 
-const DynamicPiSdkLoader = dynamic(
-  () => import("@/components/pi/PiSdkLoader"),
-  { ssr: false }
-);
+// PiSdkLoader deve essere importato dinamicamente
+const PiSdkLoader = dynamic(() => import("@/components/pi/PiSdkLoader"), {
+  ssr: false,
+});
 
 export default function HistoryPage() {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return (
-      <div className="min-h-screen p-4 bg-gray-900 text-white">
-        <div className="text-xl">Caricamento…</div>
-      </div>
-    );
-  }
-
   return (
-    <DynamicPiSdkLoader>
+    <PiSdkLoader>
       <div className="min-h-screen p-4 bg-gray-900 text-white">
         <h1 className="text-3xl font-bold mb-6">Storico IOUs</h1>
         <HistoryContent />
       </div>
-    </DynamicPiSdkLoader>
+    </PiSdkLoader>
   );
 }
