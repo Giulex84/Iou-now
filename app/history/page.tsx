ee99065051e789f31f72ed2473f78405753b21bf
Giulex84 <giulianom.gm@gmail.com>
2025-11-28 02:44:22 +0100

    Update page.tsx

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,11 +1,12 @@
+"use client";
+
 import dynamic from "next/dynamic";
 
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
-const Page = dynamic(
-  () => import("@/components/HistoryPage"),
-  { ssr: false }
-);
+const Page = dynamic(() => import("@/components/HistoryPage"), {
+  ssr: false,
+});
 
 export default Page;
