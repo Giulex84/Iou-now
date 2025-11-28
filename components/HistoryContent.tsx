38f2b08a6d444b1c3d145b57c39db77abcacc037
Giulex84 <giulianom.gm@gmail.com>
2025-11-28 16:27:27 +0100

    Update HistoryContent.tsx

--- components/HistoryContent.tsx
+++ components/HistoryContent.tsx
@@ -7,11 +7,20 @@ import IOUCard from "@/components/iou-card";
 export default function HistoryContent() {
   const { ious } = useIOUs();
 
+  if (!ious.length) {
+    return (
+      <p className="text-gray-400">
+        Nessun IOU ancora registrato.
+      </p>
+    );
+  }
+
   return (
-    <div className="space-y-4">
+    <div className="space-y-3">
       {ious.map((iou) => (
-        <IOUCard key={iou.id} {...iou} />
+        <IOUCard key={iou.id} iou={iou} />
       ))}
     </div>
   );
 }
+```0
