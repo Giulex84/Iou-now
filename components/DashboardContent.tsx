78f29cd9332c1a303d79d041efe2dadc934ce84d
Giulex84 <giulianom.gm@gmail.com>
2025-11-29 01:38:50 +0100

    Update DashboardContent.tsx

--- components/DashboardContent.tsx
+++ components/DashboardContent.tsx
@@ -4,7 +4,7 @@ import { useIOUs } from "@/components/providers/IOUProvider";
 
 export default function DashboardContent() {
   const { ious } = useIOUs();
-  
+
   return (
     <div className="text-center text-white mt-6">
       Totale IOU: {ious.length}
