9d423ec153f372becb90b41a6d4f0684b05b38a2
Giulex84 <giulianom.gm@gmail.com>
2025-11-30 00:55:53 +0100

    Create page.tsx

---
+++ app/history/page.tsx
@@ -0,0 +1,8 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
+import HistoryContent from "@/components/HistoryContent";
+
+export default function HistoryPage() {
+  return <HistoryContent />;
+}
