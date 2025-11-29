92bdfe90c15117cf468db145859f4e6046d4ef67
Giulex84 <giulianom.gm@gmail.com>
2025-11-29 02:38:12 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { IOUProvider } from "@/components/providers/IOUProvider";
+import IOUProvider from "@/components/providers/IOUProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
