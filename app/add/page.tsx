9d2dd25a9e135c1a35d0326ed1a2b5959f3a6648
Giulex84 <giulianom.gm@gmail.com>
2025-12-01 11:04:48 +0100

    Update page.tsx

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -2,12 +2,12 @@
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
-import AddForm from "@/components/AddForm";
+import AddIOUForm from "@/components/AddIOUForm";
 
 export default function AddPage() {
   return (
     <div className="min-h-screen p-4 bg-gray-900 text-white">
-      <AddForm />
+      <AddIOUForm />
     </div>
   );
 }
