75e429b433c2a1a1fd4c6e9684ce35d598c2aa02
Giulex84 <giulianom.gm@gmail.com>
2025-12-01 10:48:35 +0100

    Update page.tsx

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,8 +1,13 @@
+// app/history/page.tsx
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
 import HistoryContent from "@/components/HistoryContent";
 
 export default function HistoryPage() {
-  return <HistoryContent />;
+  return (
+    <div className="min-h-screen p-4 bg-gray-900 text-white">
+      <HistoryContent />
+    </div>
+  );
 }
