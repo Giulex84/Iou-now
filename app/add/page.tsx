2fa8704e83bf641fb047cf7fc843bc0bdcb41bbc
Giulex84 <giulianom.gm@gmail.com>
2025-12-01 10:47:56 +0100

    Update page.tsx

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -1,15 +1,13 @@
-// app/page.tsx
+// app/add/page.tsx
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
 
-import DashboardContent from "@/components/DashboardContent";
-import StartPaymentButton from "@/components/StartPaymentButton";
+import AddForm from "@/components/AddForm";
 
-export default function Page() {
+export default function AddPage() {
   return (
     <div className="min-h-screen p-4 bg-gray-900 text-white">
-      <DashboardContent />
-      <StartPaymentButton />
+      <AddForm />
     </div>
   );
 }
